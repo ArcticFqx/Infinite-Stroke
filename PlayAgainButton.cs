46b0737ad5bb9788deebf1987497f960b3847122
Morten Formo <morten@mwahaha-games.com>
2013-01-27 12:09:52 +0100

    PlayAgainButton

---
+++ PlayAgainButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayAgainButton : MonoBehaviour {
+
+	// Use this for initialization
+    void OnClick()
+    {
+        GameObject scorethingy = GameObject.FindGameObjectWithTag("scorekeeper");
+        Destroy(scorethingy);
+
+        Application.LoadLevel(0);
+    }
+}
